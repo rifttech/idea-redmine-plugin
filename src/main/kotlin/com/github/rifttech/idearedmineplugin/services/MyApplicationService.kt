bd1c4fa4595bee5d3ebf0199f9658711b4843f7c
GitHub Action <action@github.com>
2021-03-28 17:38:45 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/rifttech/idearedmineplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.rifttech.idearedmineplugin.services
+
+import com.github.rifttech.idearedmineplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/rifttech/idearedmineplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/rifttech/idearedmineplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.rifttech.idearedmineplugin.services
 
+import com.github.rifttech.idearedmineplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
